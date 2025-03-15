fe622003f4e73371289da6fbfafecca11e75f457
Battarey <killer.tim2018@gmail.com>
2025-03-15 17:04:12 +0300

    update gitignore, functions, create py file for work with db, created config file for parameters DB

--- functions.py
+++ functions.py
@@ -1,14 +1,22 @@
 import requests as rq # For site requests
 import os # To clean console
-import psycopg2 # For work PGAdmin and SQL
-
-# попробовать запрос на подключение к БД реализовать в отедльном файле
+from workWithDB import Database # For work with DB
+from config import DB_CONFIG # Config for DB
 
 def clearConsole():
     os.system('cls')
-
-def authorization(): 
-    print('q')
+    
+def authorization():
+    db = Database(DB_CONFIG)
+    db.connect()
+    try:
+        rows = db.execute_query("SELECT * FROM Users")
+        for row in rows:
+            print(row) 
+    except Exception as e:
+        print(f"An error occurred during the authorization process: {e}")
+    finally:
+        db.close()
 
 def mainMenuUser(nickname):
     print('q')
---
+++ workWithDB.py
@@ -0,0 +1,38 @@
+import psycopg2 # For work PGAdmin and SQL
+from config import DB_CONFIG # Config
+
+class Database:
+    def __init__(self, db_config):
+        self.dbname = db_config['dbname']
+        self.user = db_config['user']
+        self.password = db_config['password']
+        self.host = db_config['host']
+        self.port = db_config['port']
+        self.conn = None
+        self.cur = None
+
+    def connect(self):
+        try:
+            self.conn = psycopg2.connect(
+                dbname=self.dbname, 
+                user=self.user,  
+                password=self.password,  
+                host=self.host,          
+                port=self.port
+            )
+            self.cur = self.conn.cursor()
+        except Exception as e:
+            print(f"Error with DB! {e}")
+
+    def execute_query(self, query, params=None):
+        try:
+            self.cur.execute(query, params)
+            return self.cur.fetchall()
+        except Exception as e:
+            print(f"Error executing the query: {e}")
+
+    def close(self):
+        if self.cur:
+            self.cur.close()
+        if self.conn:
+            self.conn.close()
