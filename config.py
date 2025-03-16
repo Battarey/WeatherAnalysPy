5472c32085d4034283ae6acd4aedb8f5511bd638
Battarey <killer.tim2018@gmail.com>
2025-03-16 22:07:33 +0300

    finaly build

---
+++ config.py
@@ -0,0 +1,10 @@
+DB_CONFIG = {
+    'dbname': '-',
+    'user': 'postgres',
+    'password': '-',
+    'host': 'localhost',
+    'port': '5432',
+}
+
+API_KEY_OpenWeatherMap = '-'
+API_KEY_WeatherApi = '-'
